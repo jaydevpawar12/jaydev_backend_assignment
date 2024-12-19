cf75ff287fc8c7f44f64b082ad167cdfc4e2ad95
jaydevpawar12 <107975266+jaydevpawar12@users.noreply.github.com>
2024-12-19 18:49:29 +0530

    redis upadte

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,7 @@
 const { default: Redis } = require("ioredis")
 require("dotenv").config()
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST ||"127.0.0.1" ,
-    port: process.env.REDIS_PORT ||"6379"
-})
+const redis = new Redis(process.env.REDIS_URL)
 redis.on('error', (err) => {
     console.error("Redis connection error:", err);
 })
